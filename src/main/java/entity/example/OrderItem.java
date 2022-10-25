ad7c13fb031f9da971c710df7e6689dfab2df1e0
ckr3453 <ckr3453@gmail.com>
2022-10-25 23:50:53 +0900

    Add: 생성일, 생성자, 수정일, 수정자 멤버를 BaseEntity 로부터 상속받음

--- src/main/java/entity/example/OrderItem.java
+++ src/main/java/entity/example/OrderItem.java
@@ -3,7 +3,7 @@ package entity.example;
 import javax.persistence.*;
 
 @Entity
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name="ORDER_ITEM_ID")
